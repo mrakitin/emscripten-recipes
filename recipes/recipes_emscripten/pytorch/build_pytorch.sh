02557be9b0b2ce1cd72f3b43ba1821b77ebe4ca1
Max Rakitin <max.rakitin@exabyte.io>
2024-03-19 02:46:33 -0400

    Build pytorch

---
+++ recipes/recipes_emscripten/pytorch/build_pytorch.sh
@@ -0,0 +1,13 @@
+set -x
+if [[ "$megabuild" == true ]]; then
+  source $RECIPE_DIR/build.sh
+  pushd $SP_DIR/torch
+  for f in bin/* lib/* share/* include/*; do
+    if [[ -e "$PREFIX/$f" ]]; then
+      rm -rf $f
+      ln -sf $PREFIX/$f $PWD/$f
+    fi
+  done
+else
+  $PREFIX/bin/python -m pip install --no-deps torch-*.whl
+fi
